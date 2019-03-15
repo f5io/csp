4c5fb1e00037a44ffff89175078399f038c37b10
jfet97 <andrysimo1997@gmail.com>
2019-03-15 19:03:39 +0100

    add test for async iterable interface

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -10,7 +10,7 @@ const msg = (() => {
 
 test('[csp] channel', t => {
   const chan = channel<number>();
-  t.equal(Object.getOwnPropertySymbols(chan).length, 4, 'should contain 4 symbol properties');
+  t.equal(Object.getOwnPropertySymbols(chan).length, 5, 'should contain 5 symbol properties');
   t.equal(Object.keys(chan).length, 0, 'should not expose properties normally');
   t.end();
 });
@@ -39,7 +39,7 @@ test('[csp] alts', t => {
 
 test('[csp] drain', async t => {
   const chan = channel<number>();
-  const messages = [ msg(), msg(), msg(), msg(), msg() ];
+  const messages = [msg(), msg(), msg(), msg(), msg()];
   messages.forEach(m => put(chan, m));
   const res = drain(chan);
   t.ok(res instanceof Promise, 'should return an instance of a Promise');
@@ -63,6 +63,15 @@ test('[csp] put, already taking', async t => {
   const result = take(chan);
   await put(chan, m);
   const res = await result;
+  t.equal(res, m, 'should resolve the correct value');
+  t.end();
+});
+
+test('[csp] take with asynciterable interface', async t => {
+  const chan = channel<number>();
+  const m = msg();
+  put(chan, m);
+  const res = (await chan[Symbol.asyncIterator]().next()).value;
   t.equal(res, m, 'should resolve the correct value');
   t.end();
 });
@@ -94,9 +103,9 @@ test('[csp] select Array, chan1 ready', async t => {
   const chan1 = channel<number>();
   const chan2 = channel<string>();
   const m = msg();
-  const result = select<Input>([ chan1, chan2 ]);
+  const result = select<Input>([chan1, chan2]);
   await put(chan1, m);
-  const [ id, res ] = await result;
+  const [id, res] = await result;
   t.equal(id, 0, 'should receive the correct id');
   t.equal(res, m, 'should receive the correct value');
   t.end();
@@ -107,9 +116,9 @@ test('[csp] select Array, chan2 ready', async t => {
   const chan2 = channel();
   const m = msg();
   const putter = put(chan2, m);
-  const result = select([ chan1, chan2 ]);
+  const result = select([chan1, chan2]);
   await putter;
-  const [ id, res ] = await result;
+  const [id, res] = await result;
   t.equal(id, 1, 'should receive the correct id');
   t.equal(res, m, 'should receive the correct value');
   t.end();
@@ -121,9 +130,9 @@ test('[csp] select Map, chan1 ready', async t => {
   const m = msg();
   const key1 = Symbol();
   const key2 = Symbol();
-  const result = select<Input>(new Map<Symbol, Channel<Input>>([ [ key1, chan1 ], [ key2, chan2 ] ]));
+  const result = select<Input>(new Map<Symbol, Channel<Input>>([[key1, chan1], [key2, chan2]]));
   await put(chan1, m);
-  const [ id, res ] = await result;
+  const [id, res] = await result;
   t.equal(id, key1, 'should receive the correct id');
   t.equal(res, m, 'should receive the correct value');
   t.end();
@@ -136,9 +145,9 @@ test('[csp] select Map, chan2 ready', async t => {
   const putter = put(chan2, m);
   const key1 = Symbol();
   const key2 = Symbol();
-  const result = select(new Map([ [ key1, chan1 ], [ key2, chan2 ] ]));
+  const result = select(new Map([[key1, chan1], [key2, chan2]]));
   await putter;
-  const [ id, res ] = await result;
+  const [id, res] = await result;
   t.equal(id, key2, 'should receive the correct id');
   t.equal(res, m, 'should receive the correct value');
   t.end();
@@ -150,7 +159,7 @@ test('[csp] select Object, chan1 ready', async t => {
   const m = msg();
   const result = select<Input>({ a: chan1, b: chan2 });
   await put(chan1, m);
-  const [ id, res ] = await result;
+  const [id, res] = await result;
   t.equal(id, 'a', 'should receive the correct id');
   t.equal(res, m, 'should receive the correct value');
   t.end();
@@ -163,7 +172,7 @@ test('[csp] select Object, chan2 ready', async t => {
   const putter = put(chan2, m);
   const result = select({ a: chan1, b: chan2 });
   await putter;
-  const [ id, res ] = await result;
+  const [id, res] = await result;
   t.equal(id, 'b', 'should receive the correct id');
   t.equal(res, m, 'should receive the correct value');
   t.end();
@@ -173,9 +182,9 @@ test('[csp] select Set, chan1 ready', async t => {
   const chan1 = channel<number>();
   const chan2 = channel<string>();
   const m = msg();
-  const result = select<Input>(new Set([ chan1, chan2 ]));
+  const result = select<Input>(new Set([chan1, chan2]));
   await put(chan1, m);
-  const [ id, res ] = await result;
+  const [id, res] = await result;
   t.equal(id, chan1, 'should receive the correct id');
   t.equal(res, m, 'should receive the correct value');
   t.end();
@@ -186,9 +195,9 @@ test('[csp] select Set, chan2 ready', async t => {
   const chan2 = channel();
   const m = msg();
   const putter = put(chan2, m);
-  const result = select(new Set([ chan1, chan2 ]));
+  const result = select(new Set([chan1, chan2]));
   await putter;
-  const [ id, res ] = await result;
+  const [id, res] = await result;
   t.equal(id, chan2, 'should receive the correct id');
   t.equal(res, m, 'should receive the correct value');
   t.end();
